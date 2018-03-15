a6fd4c1f77008a15aa6c2098b25607ba9cd4785c
Jai Kumar <jaivardhan@gmail.com>
2018-03-15 01:05:25 -0700

    comp;ete token.sol

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -9,5 +9,43 @@ import './interfaces/ERC20Interface.sol';
  */
 
 contract Token is ERC20Interface {
-	// YOUR CODE HERE
+	
+  mapping(address => uint256) public balances;
+  mapping(address => mapping (address => uint256)) public allowed;
+  
+  event Transfer(address indexed _from, address indexed _to, uint256 _value)
+  event Approval(address indexed _owner, address indexed _spender, uint256 _value)
+  
+  function balanceOf(address _owner) constant returns (uint256 balance) {
+    return balances[_owner];
+  }
+
+  function transfer(address _to, uint256 _value) returns (bool success) {
+    //require(_value <= totalSupply);
+    require(balances[msg.sender] >= _value);
+    balances[msg.sender] -= _value
+    balances[_to] += _value;
+    Transfer(msg.sender, _to, _value);
+    return true;
+  }
+
+  function transferFrom(address _from, address _to, uint256 _value) returns (bool success){
+    uint256 allowed_value = allowed[msg.sender][_from];
+    require(balances[_from] >= _value && allowed_value >= _value);
+    balances[_from] -= _value; 
+    balances[_to] += _value; 
+    allowed[msg.sender][_from] -= value;
+    return true;
+  }
+
+  function approve(address _spender, uint256 _value) returns (bool success){
+    allowed[msg.sender][_spender] = _value
+    Approval(msg.sender, _spender, _value);
+    return true;
+  }
+
+  function allowance(address _owner, address _spender) view returns (uint256 remaining){
+    return allowed[_owner][_spender];
+  }
+
 }
